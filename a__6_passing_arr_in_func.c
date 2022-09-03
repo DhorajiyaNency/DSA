008c161b787c45dc2c8ce7113fa01a38934c4490
Dhorajiya Nency <109361169+DhorajiyaNency@users.noreply.github.com>
2022-09-03 10:42:43 +0530

    02/09/2022

---
+++ a__6_passing_arr_in_func.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+    int a[5]={1,2,3,4,5};
+    void test(int []);  // function declaration
+    test(a);                // function calling 
+    printf("\n main : ");
+    for(int i=0;i<5;i++)
+        printf(" %6d",a[i]);
+}
+void test(int A[])  // function definition
+{
+    printf("\n test : ");
+    for(int i=0;i<5;i++)
+    {
+        A[i]*=A[i];
+        printf(" %6d",A[i]);
+    }
+}
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a=10;
+//     void test(int);  // function declaration
+//     test(a);                // function calling 
+//     printf(" %6d",a);
+// }
+// void test(int A)  // function definition
+// {
+//     printf("\n In test : ");
+//     A*=A;
+//     printf(" %6d",A);
+// }
+
+// in passing array --> pass address
+// in passing value --> pass reference
