a5eee887927764c618ecc9251bab3dc0fc04c4a1
Dhorajiya Nency <109361169+DhorajiyaNency@users.noreply.github.com>
2022-09-10 09:55:11 +0530

    06/09/2022

--- b__2_Quick_sorting.c
+++ b__2_Quick_sorting.c
@@ -73,3 +73,61 @@ int main()
 //  inside quick :  22  34  37  40  48  61  100  82  86  83
 //  inside quick :  22  34  37  40  48  61  83  82  86  100
 //  Order of sorted Elements :  22  34  37  40  48  61  82  83  86  100
+
+
+/*
+#include<stdio.h>
+#include<stdlib.h>
+int n,i,j,pivot,temp;
+void print(int a[])
+{
+    int i;
+    for(int i=0;i<n;i++)
+        printf(" %d ",a[i]);
+}
+void swap_func(int temp,int a[])
+{
+    temp=a[pivot];
+    a[pivot]=a[j];
+    a[j]=temp;
+}
+void quickSort(int a[],int first,int last)
+{
+    if(first<last)
+    {
+        pivot=first;
+        i=first;
+        j=last;
+        while (i<j)
+        {
+            printf("\n inside quick : ");
+            print(a);
+            while (a[i]<=a[pivot]&&i<last)
+            i++;
+            while (a[j]>a[pivot])
+            j--;
+            if(i<j)
+                swap_func(temp,a);
+        }
+            swap_func(temp,a);
+            quickSort(a,first,j-1);
+            quickSort(a,j+1,last);    
+    }
+}
+int main()
+{
+    int i,a[25],min=20,max=101;
+    printf("how many element ? : ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+        a[i]=(rand()%(max-min+1))+min;
+    print(a);
+    quickSort(a,0,n-1);
+    printf("\n Order of sorted Elements : ");
+    for ( i = 0; i < n; i++)
+    {
+        printf(" %d ",a[i]);
+    }
+    return 0;   
+}
+*/
